4e2c703a2c9c5dd5db2ba2dbc826ca7c61cc3bcf
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2021-05-24 12:56:04 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-post1 = Post.create(
-  title: 'The 2021 Independent Watchmaking Fantasy Draft',
-  description: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.',
-  content: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.'
-)
+5.times.each |i| do
+  post1 = Post.create(
+    title: "#{i} - The 2021 Independent Watchmaking Fantasy Draft",
+    description: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.',
+    content: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.'
+  )
 
-post1.image.attach(io: File.open('/app/spec/fixtures/images/1.jpg'), filename: '1.jpg')
+  post1.image.attach(io: File.open('/app/spec/fixtures/images/1.jpg'), filename: '1.jpg')
 
-post2 = Post.create(
-  title: ' The Lindbergh Hour Angle Watch, Possibly The Most Toolish Tool Watch Ever Made',
-  description: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.',
-  content: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.'
-)
-post2.image.attach(io: File.open('/app/spec/fixtures/images/2.jpg'), filename: '2.jpg')
+  post2 = Post.create(
+    title: "#{i} = The Lindbergh Hour Angle Watch, Possibly The Most Toolish Tool Watch Ever Made",
+    description: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.',
+    content: 'The wonderful, weird, and wild of contemporary horology go head-to-head-to-head.'
+  )
+  post2.image.attach(io: File.open('/app/spec/fixtures/images/2.jpg'), filename: '2.jpg')
+end
 
